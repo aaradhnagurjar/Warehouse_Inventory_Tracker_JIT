0a6c20d8b36bc273c079da9e3cfb1727afc16a31
aaradhnagurjar <aaradhnagurjarg@gmail.com>
2025-10-23 23:57:22 +0530

    Reupload

---
+++ WarehouseInventoryTracker/src/main/java/com/warehouse/WarehouseInventoryTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseInventoryTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseInventoryTrackerApplication.class, args);
+	}
+
+}
---
+++ WarehouseInventoryTracker/src/test/java/com/warehouseinventory/WarehouseInventoryTracker/WarehouseInventoryTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warehouseinventory.WarehouseInventoryTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseInventoryTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
